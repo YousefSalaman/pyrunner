482d1724fa820c8a0934aae4a420b27a20b89ab5
osvaldo <aquino35173@gmail.com>
2021-02-02 07:05:59 -0400

    More tester optimization

---
+++ testers/cls_prop_test.py
@@ -0,0 +1,89 @@
+from Simupynk.utils.cls_prop import classproperty, CPEnabled, CPEnabledMeta
+from unittest import TestCase
+import pytest
+
+
+class Foo(CPEnabled):  # testing CPEnabled
+    _bar = 0
+
+    @classproperty
+    @classmethod
+    def bar(cls):
+        """
+        Test class property
+        """
+        return cls._bar
+
+    @bar.setter
+    def bar(cls, value):
+        cls._bar = value
+
+    @bar.getter
+    def bar(cls):
+        return 1
+
+    @bar.deleter
+    @classmethod
+    def bar(cls):
+        print("Deleting...")
+        del cls._bar
+
+
+class FooMeta(metaclass=CPEnabledMeta):  # testing CPEnabledMeta
+    _bar = 0
+
+    @classproperty
+    @classmethod
+    def bar(cls):
+        """
+        Test class property
+        """
+        return cls._bar
+
+    @bar.setter
+    def bar(cls, value):
+        cls._bar = value
+
+    @bar.getter
+    def bar(cls):
+        return 1
+
+    @bar.deleter
+    @classmethod
+    def bar(cls):
+        print("Deleting...")
+        del cls._bar
+
+def test_getter():
+    object = Foo()
+    assert object.bar == 1, "The value of the func should be 1."
+
+    object = FooMeta()
+    assert object.bar == 1, "The value of the func should be 1."
+
+def test_setter():
+    object = Foo()
+    object.bar = 1
+    assert object.bar == 1, "The expected result is 1"
+
+    Foo.bar = 2
+    assert Foo.bar, 1 == "The expected result is 1"
+
+    object = FooMeta()
+    object.bar = 1
+    assert object.bar == 1, "The expected result is 1"
+    Foo.bar = 2
+    assert Foo.bar == 1, "The expected result is 1"
+
+def test_del():
+    # testing switch value error
+    with pytest.raises(AttributeError):
+        object = Foo()
+        del object.bar
+        print(Foo._bar)  # It's suppose to give an error since the property is deleted by the deleter
+        print(Foo.bar)  # This will also yield an error
+
+        object = FooMeta()
+        del object.bar
+        print(Foo._bar)  # It's suppose to give an error since the property is deleted by the deleter
+        print(Foo.bar)  # This will also yield an error
